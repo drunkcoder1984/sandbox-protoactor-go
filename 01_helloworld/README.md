fd5391535d3d963a678f3f7b6d13de226cbba13d
kashiwabarak <kashiwabarak@marv.jp>
2024-05-16 18:03:11 +0900

    公式のhello worldを試す

---
+++ 01_helloworld/README.md
@@ -0,0 +1,6 @@
+# actor-helloworld
+## 概要
+golangでproto.actorの使い方を理解するために色々試すリポジトリ
+## メモ
+- 詳しい説明は以下のリンクを参照
+  - https://github.com/asynkron/protoactor-go/blob/dev/examples/actor-helloworld
---
+++ 01_helloworld/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log/slog"
+
+	console "github.com/asynkron/goconsole"
+	"github.com/asynkron/protoactor-go/actor"
+)
+
+type (
+	hello      struct{ Who string }
+	helloActor struct{}
+)
+
+func (state *helloActor) Receive(context actor.Context) {
+	switch msg := context.Message().(type) {
+	case *hello:
+		context.Logger().Info("Hello", " %v", slog.String("who", msg.Who))
+	}
+}
+
+func main() {
+	system := actor.NewActorSystem()
+	props := actor.PropsFromProducer(func() actor.Actor { return &helloActor{} })
+
+	pid := system.Root.Spawn(props)
+	system.Root.Send(pid, &hello{Who: "Roger"})
+	_, _ = console.ReadLine()
+}
